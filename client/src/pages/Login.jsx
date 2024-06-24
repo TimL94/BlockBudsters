c9b7f99ebb2bc549d84ce572d37677c4ac35f009
TimL94 <soccerguy159@gmail.com>
2024-06-23 20:25:29 -0400

    added login form to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,11 @@
-import { Container } from "@mui/material";
+import {
+    Container,
+    Box,
+    Paper,
+    TextField,
+    Button,
+    Link
+} from "@mui/material";
 
 
 function Login() {
@@ -8,10 +15,70 @@ function Login() {
                 position: 'relative',
                 display: 'flex',
                 flexDirection: 'column',
-                alignItems: 'center'
+                alignItems: 'center',
+                mt: 2
             }}
         >
-            <h1>Login</h1>
+             <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                    m: 1,
+                    width: {xs:'75vw', md:'45vw'},
+                    textAlign:'center'
+                    },
+                }}
+            >
+                <Paper 
+                    elevation={24}
+                    sx={{ borderRadius:5}}
+                >
+                    <h2>
+                        Login
+                    </h2>
+                    Sign in here!
+                    <Box
+                        component="form"
+                        sx={{
+                            '& .MuiTextField-root': { m: 1, width: '25ch' },
+                            mt:3,
+                            display:'flex',
+                            flexDirection: 'column'
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <Box sx={{mb:2}}>
+                            <TextField
+                            required
+                            color="secondary"
+                            label="Email"
+                            defaultValue=" "
+                            focused
+                            />
+                        </Box>
+                        <Box>
+                            <TextField
+                            required
+                            color='secondary'
+                            label="Password"
+                            defaultValue=" "
+                            focused
+                            />
+                        </Box>
+                        <Box sx={{mt:2, mb:3}}>
+                            <Button variant="contained" sx={{width:'40%', m: 'auto', borderRadius:5}}>
+                                Submit
+                            </Button>
+                        </Box>
+                        <Box sx={{mb:2}}>
+                            Don't have an account?<br></br>
+                            Sign up <Link>Here!</Link>
+                        </Box>
+                    </Box>
+                </Paper>
+            </Box>
         </Container>
     )
 }
