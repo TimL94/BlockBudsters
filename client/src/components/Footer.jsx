0ff2adc8f7b56d28a2f3a6222c281b1d8438c570
TimL94 <soccerguy159@gmail.com>
2024-06-26 16:09:21 -0400

    adjusted footer list items to be centered in larger screen sizes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,8 @@ import {
     List,
     ListItem,
     Typography,
-    Grid
+    Grid,
+    Container
 } from '@mui/material';
 
 const Footer = () => {
@@ -15,7 +16,10 @@ const Footer = () => {
                 flexGrow: 0, 
                 position: 'fixed', 
                 bottom: 0, 
-                width: '100%'
+                width: '100%', 
+                mb: 2,
+                py: 2,
+                bgcolor: 'background.paper',
             }}
         >
             <Divider>
@@ -23,42 +27,44 @@ const Footer = () => {
                     603 Bar and Lounge
                 </Typography>
             </Divider>
-            <Box sx={{ p: 2, textAlign: 'center' }}>
-                <Typography variant="subtitle2" sx={{ fontSize: '0.7rem', fontWeight: 300, mb: 1 }}>
-                    Kitchen Hours
-                </Typography>
-                <Grid container spacing={1} justifyContent="center">
-                    <Grid item xs={6}>
-                        <List sx={{ p: 0 }}>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Sunday: 11:00am-10:00pm
-                            </ListItem>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Monday: Closed
-                            </ListItem>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Tuesday: 11:00am-10:00pm
-                            </ListItem>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Wednesday: 11:00am-10:00pm
-                            </ListItem>
-                        </List>
+            <Container>
+                <Box sx={{ textAlign: 'center', mt: 1 }}>
+                    <Typography variant="subtitle2" sx={{ fontSize: '0.7rem', fontWeight: 300, mb: 1 }}>
+                        Kitchen Hours
+                    </Typography>
+                    <Grid container justifyContent="center" spacing={2}>
+                        <Grid item>
+                            <List sx={{ p: 0 }}>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Sunday: 11:00am-10:00pm
+                                </ListItem>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Monday: Closed
+                                </ListItem>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Tuesday: 11:00am-10:00pm
+                                </ListItem>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Wednesday: 11:00am-10:00pm
+                                </ListItem>
+                            </List>
+                        </Grid>
+                        <Grid item>
+                            <List sx={{ p: 0 }}>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Thursday: 11:00am-10:00pm
+                                </ListItem>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Friday: 11:00am-11:00pm
+                                </ListItem>
+                                <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
+                                    Saturday: 11:00am-11:00pm
+                                </ListItem>
+                            </List>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <List sx={{ p: 0 }}>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Thursday: 11:00am-10:00pm
-                            </ListItem>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Friday: 11:00am-11:00pm
-                            </ListItem>
-                            <ListItem sx={{ fontSize: '0.6rem', fontWeight: 300, py: 0.5 }}>
-                                Saturday: 11:00am-11:00pm
-                            </ListItem>
-                        </List>
-                    </Grid>
-                </Grid>
-            </Box>
+                </Box>
+            </Container>
         </Box>
     )
 }
