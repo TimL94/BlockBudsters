c624a251af3e8c5ef79dcbe803cd65540bc28ca7
TimL94 <soccerguy159@gmail.com>
2025-04-09 16:51:11 -0400

    hopeful fix

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const startApolloServer = async () => {
 
   // Enable CORS before routes
   app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000"
+    origin: process.env.CLIENT_URL || "*"
   }));
   
   app.use(express.urlencoded({ extended: false }));
